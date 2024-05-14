3bd57af2998aee4ae29b1111f2a0a37471a27a8c
Lucas Marinho <lucas.marinho@pontestur.com.br>
2024-05-14 15:04:00 -0300

    Estilização em progresso

---
+++ meu_projeto/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './meu_app/templates/**/*.html', // Substitua 'meu_app' pelo nome da sua aplicação
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
