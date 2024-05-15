d0b43d4770311b16829c3825c63225da7f721464
Lucas Marinho <lucas.marinho@pontestur.com.br>
2024-05-15 15:42:26 -0300

    Site funcional

--- meu_projeto/meu_app/views.py
+++ meu_projeto/meu_app/views.py
@@ -3,10 +3,45 @@ from django.http import HttpResponse
 from .models import Compra
 from .forms import CompraForm
 import json
+from decimal import Decimal
 
 def lista_compras(request):
     compras = Compra.objects.all()
-    return render(request, 'lista_compras.html', {'compras': compras})
+
+    # Obter os filtros dos cookies
+    filtros = {
+        'nome': request.COOKIES.get('nome', 'off'),
+        'email': request.COOKIES.get('email', 'off'),
+        'numero': request.COOKIES.get('numero', 'off'),
+        'data_compra': request.COOKIES.get('data_compra', 'off'),
+        'pacote': request.COOKIES.get('pacote', 'off'),
+        'valor': request.COOKIES.get('valor', 'off'),
+        'taxa_catarse': request.COOKIES.get('taxa_catarse', 'off'),
+        'faturamento': request.COOKIES.get('faturamento', 'off'),
+    }
+
+    # Aplicar os filtros à consulta (adaptando para propriedades calculadas)
+    if filtros['nome'] == 'on':
+        compras = compras.exclude(nome__isnull=True).exclude(nome__exact='')
+    if filtros['email'] == 'on':
+        compras = compras.exclude(email__isnull=True).exclude(email__exact='')
+    if filtros['numero'] == 'on':
+        compras = compras.exclude(numero__isnull=True)
+    if filtros['data_compra'] == 'on':
+        compras = compras.exclude(data_compra__isnull=True)
+    if filtros['pacote'] == 'on':
+        compras = compras.exclude(pacote__isnull=True).exclude(pacote__exact='')
+    if filtros['valor'] == 'on':
+        compras = compras.exclude(valor__isnull=True)
+    if filtros['taxa_catarse'] == 'on':
+        compras = [c for c in compras if c.taxa_catarse > Decimal('0.00')] # Filtra propriedades
+    if filtros['faturamento'] == 'on':
+        compras = [c for c in compras if c.faturamento > Decimal('0.00')] # Filtra propriedades
+
+    # Renderizar o template com as compras filtradas e os filtros atuais
+    return render(request, 'lista_compras.html', {'compras': compras, 'filtros': filtros})
+
+# ... (restante do código views.py)
 
 def nova_compra(request):
     if request.method == 'POST':
@@ -40,6 +75,17 @@ def deletar_compra(request, pk):
 
 def download_compras(request):
     compras = Compra.objects.all()
+
+    filtros = {
+        'nome': request.COOKIES.get('nome', 'off'),
+        'email': request.COOKIES.get('email', 'off'),
+        'numero': request.COOKIES.get('numero', 'off'),
+        'data_compra': request.COOKIES.get('data_compra', 'off'),
+        'pacote': request.COOKIES.get('pacote', 'off'),
+        'valor': request.COOKIES.get('valor', 'off'),
+        'taxa_catarse': request.COOKIES.get('taxa_catarse', 'off'),
+        'faturamento': request.COOKIES.get('faturamento', 'off'),
+    }
     
     # Filtrar os dados com base nas colunas selecionadas
     data = []
